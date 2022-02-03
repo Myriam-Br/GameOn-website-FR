d1a6dfc6b3602788c7ceedf544285e879a0797f6
Myriam Boukarniaa <myriam.boukarniaa@hotmail.com>
2022-02-03 19:12:59 +0100

    validation formulaire terminée, correction label html terminée

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,11 +14,6 @@ const formData = document.querySelectorAll(".formData");
 
 const modalValidation = document.getElementsByClassName('container-msg');
 const btnSubmit = document.getElementsByClassName("btn-submit");
-const msgValidation = document.getElementsByClassName("container-msg");
-const signUpForm = document.getElementsByClassName("signup-form");
-
-
-
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -47,7 +42,7 @@ function validate() {
   const alertLastName = document.getElementById('alert-lastname');
 
   var email = document.getElementById('email'); 
-  const emailFilter =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailFilter = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const alertEmail = document.getElementById('alert-email'); 
 
   var birthdate = document.getElementById('birthdate');
@@ -55,9 +50,12 @@ function validate() {
 
   var quantity = document.getElementById('quantity'); 
   const alertQuantity = document.getElementById('alert-quantity'); 
-  
+
   const locationSelected = document.querySelector('input[name="location"]:checked');
-  const termesOfServices = document.querySelector('input[id="checkbox1"]:checked')
+  const alertLocation = document.getElementById('alert-location'); 
+
+  const termesOfServices = document.querySelector('input[id="checkbox1"]:checked');
+  const alertTOS = document.getElementById('alert-tos'); 
   
   var sendForm = true;
 
@@ -78,77 +76,89 @@ function validate() {
   alertFirstName.innerHTML = 'votre prénom doit contenir au moins 2 caractères';
   sendForm = false 
  } else{
-   console.log('prénom valide');
-   console.log(firstName.length);
+   //console.log('prénom valide');
+   //console.log(firstName.length);
+   alertFirstName.innerHTML = "" 
  }
 
 //lastname validation
  if( lastName == "") {
-  console.log(" veuillez entrer votre nom");  
+  //console.log(" veuillez entrer votre nom");  
+  alertLastName.innerHTML = "veuillez entrer votre nom"
   sendForm = false 
  } else if(lastName.length < 2) {
-  console.log('votre nom doit contenir au moins 2 caractère');
+  //console.log('votre nom doit contenir au moins 2 caractère');
+  alertLastName.innerHTML = "votre nom doit contenir au moins 2 caractère"
   sendForm = false 
  } else{
-   console.log('nom valide');
-   console.log(lastName.length);
+   //console.log('nom valide');
+   //console.log(lastName.length);
+   alertLastName.innerHTML = ""
+   
  }
 
 //email validation
 if(email == ""){
-  console.log("veuillez entrer votre adresse email");
+  //console.log("veuillez entrer votre adresse email");
+  alertEmail.innerHTML = "veuillez entrer votre adresse email"
   sendForm = false 
 }else if(!email.match(emailFilter)) {
-  console.log("veuillez entrer une adresse email valide");
+  //console.log("veuillez entrer une adresse email valide");
+  alertEmail.innerHTML = "veuillez entrer une adresse email valide"
   sendForm = false 
   //utilisation de la validation email dans html -> pas fiable
 }else{
-  console.log('email valide');
+  //console.log('email valide');
+  alertEmail.innerHTML = ""
 }
 
 
 //birthdate validation
 if(birthdate == ""){
-  console.log("veuillez entrer votre date de naissance");
+  //console.log("veuillez entrer votre date de naissance");
+  alertBirthdate.innerHTML = "veuillez entrer votre date de naissance"
   sendForm = false
 } else {
-  console.log("date de naissance valide");
+  //console.log("date de naissance valide");
+  alertBirthdate.innerHTML = ""
 }
 
 //quantity validation
 if(quantity == ""){
-  console.log("veuillez entrer une valeur");
+  //console.log("veuillez entrer une valeur");
+  alertQuantity.innerHTML = "veuillez entrer une valeur"
   sendForm = false
 } else {
-  console.log("valeur valide");
+  //console.log("valeur valide");
+  alertQuantity.innerHTML = ""
 }
 
 
 //select city validation
 if(locationSelected == null){
-  console.log("select a location");
+  //console.log("select a location");
+  alertLocation.innerHTML = "sélectionnez une ville"
   sendForm = false;
 } else{
-  console.log("location selected successfully"); 
-}
-
-if(termesOfServices == null) {
-  console.log("vous devez accepter les conditions pour poursuivre votre inscription");
-  sendForm = false;
-} else {
-  console.log("condition acceptées");
+  //console.log("location selected successfully"); 
+  alertLocation.innerHTML = ""
 }
 
 
 //termes validation
-
-
-
-//console.log('Merci de votre inscription');
- return sendForm 
+if(termesOfServices == null) {
+  //console.log("vous devez accepter les conditions pour poursuivre votre inscription");
+  alertTOS.innerHTML = "vous devez accepter les conditions d'utilisation pour poursuivre votre inscription"
+  sendForm = false;
+} else {
+  //console.log("condition acceptées");
+  alertTOS.innerHTML = ""
 }
 
-// test btn submit 
+ return sendForm; 
+}
+
+
 
 
 
